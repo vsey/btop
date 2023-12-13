4795307f51c6720e6ef06b05c521875b10bc70eb
jkre <usokk@student.kit.edu>
2023-12-13 18:51:44 +0100

    consider wattage at width caclulation

--- src/btop_draw.cpp
+++ src/btop_draw.cpp
@@ -724,9 +724,9 @@ namespace Cpu {
 				old_status = status;
 				const string str_time = (seconds > 0 ? sec_to_dhms(seconds, true, true) : "");
 				const string str_percent = to_string(percent) + '%';
-				const string str_watt = floating_humanizer(watt, true);
+				const string str_watt = to_string(watt) + "W";;
 				const auto& bat_symbol = bat_symbols.at((bat_symbols.contains(status) ? status : "unknown"));
-				const int current_len = (Term::width >= 100 ? 11 : 0) + str_time.size() + str_percent.size() + to_string(Config::getI("update_ms")).size();
+				const int current_len = (Term::width >= 100 ? 11 : 0) + str_time.size() + str_percent.size() + to_string(Config::getI("update_ms")).size() + str_watt.size();
 				const int current_pos = Term::width - current_len - 17;
 
 				if ((bat_pos != current_pos or bat_len != current_len) and bat_pos > 0 and not redraw)
